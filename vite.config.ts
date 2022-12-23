66058cfab38fee1b37ac22b2c34c5d2c22b1e2b4
Anthony Smith <tony.smith@calrec.com>
2022-12-23 21:10:40 +0000

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from "path";
+
+const root = path.resolve(__dirname, "./packages/app/canalMap");
+const src = path.resolve(root, "src");
+const publicDir = path.resolve(root, "public");
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: src,
+  publicDir,
+  plugins: [react()],
+})
