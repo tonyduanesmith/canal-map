c26afee579534d0b7f2969e26074221bcce604b5
Anthony Smith <tonyduanesmith@me.com>
2023-05-08 19:23:55 +0100

    refactor: remove unused code and update when annotations change

--- packages/atoms/map/Map.tsx
+++ packages/atoms/map/Map.tsx
@@ -18,50 +18,51 @@ const Map = ({ token, id, showsUserLocation = false, annotations, overlays }: Pr
 
   const isLoaded = useIsMapkitLoaded({ token: import.meta.env.VITE_TOKEN });
 
-  if (isLoaded) {
-  }
-
   useEffect(() => {
-    const main = async () => {
-      // get current location
-      const result = await gettingGeoCoordinates();
-      const { coords } = result;
-      const currentRegion = new mapkit.CoordinateRegion(
-        new mapkit.Coordinate(coords.latitude, coords.longitude),
-        new mapkit.CoordinateSpan(0.167647972, 0.354985255),
-      );
-
-      // Create a map in the element whose ID is "map-container"
-      if (mapRef.current === undefined) {
-        mapRef.current = new mapkit.Map(id);
-      }
-      if (mapRef.current) {
-        mapRef.current.addOverlays(overlays);
-        mapRef.current.addAnnotations(annotations);
-        mapRef.current.annotationForCluster = clusterAnnotation => {
-          if (clusterAnnotation.clusteringIdentifier === "lock") {
-            const angle = clusterAnnotation.memberAnnotations[0].data?.angle ?? 0;
-            return new mapkit.ImageAnnotation(clusterAnnotation.coordinate, {
-              url: {
-                1: lockClusterImageArray[angle],
-              },
-              size: {
-                width: 30,
-                height: 30,
-              },
-              anchorOffset: new DOMPoint(0, -15),
-              animates: false,
-            });
-          }
-        };
-        mapRef.current.region = currentRegion;
-        mapRef.current.showsUserLocation = showsUserLocation;
-      }
-    };
     if (isLoaded) {
-      main();
+      const initializeMap = async () => {
+        // get current location
+        const result = await gettingGeoCoordinates();
+        const { coords } = result;
+        const currentRegion = new mapkit.CoordinateRegion(
+          new mapkit.Coordinate(coords.latitude, coords.longitude),
+          new mapkit.CoordinateSpan(0.167647972, 0.354985255),
+        );
+
+        // Create a map in the element whose ID is "map-container"
+        if (mapRef.current === undefined) {
+          mapRef.current = new mapkit.Map(id);
+        }
+        if (mapRef.current) {
+          mapRef.current.annotationForCluster = clusterAnnotation => {
+            if (clusterAnnotation.clusteringIdentifier === "lock") {
+              const angle = clusterAnnotation.memberAnnotations[0]?.data?.angle ?? 0;
+              return new mapkit.ImageAnnotation(clusterAnnotation.coordinate, {
+                url: {
+                  1: lockClusterImageArray[angle],
+                },
+                size: {
+                  width: 30,
+                  height: 30,
+                },
+                anchorOffset: new DOMPoint(0, -15),
+                animates: false,
+              });
+            }
+          };
+          mapRef.current.region = currentRegion;
+          mapRef.current.showsUserLocation = showsUserLocation;
+
+          // Add overlays and annotations to the map
+          mapRef.current.addOverlays(overlays);
+          mapRef.current.addAnnotations(annotations);
+        }
+      };
+
+      initializeMap();
     }
-  }, [isLoaded]);
+  }, [isLoaded, annotations, overlays]);
+
   return <StyledMapContainer id={id} />;
 };
 
